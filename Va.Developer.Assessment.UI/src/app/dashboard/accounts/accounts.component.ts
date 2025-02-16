2c25ec0c6f12adcbb1ca36c315c31f8a15fa3d99
Moshele Seoka <seokamk@yahoo.com>
2025-02-16 09:18:01 +0200

    Refactor: Added Custom Paths

---
+++ Va.Developer.Assessment.UI/src/app/dashboard/accounts/accounts.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: './accounts.component.html',
+    selector: 'app-accounts',
+    standalone: true,
+})
+export class AccountsComponent {}
---
+++ Va.Developer.Assessment.UI/src/app/dashboard/persons/persons.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from "@angular/common";
+import { HttpClientModule, HttpErrorResponse } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { User } from "@models/user.model";
+import { PersonService } from "@services/person.service";
+@Component({
+    selector: 'app-persons',
+    templateUrl: './persons.component.html',
+    standalone: true,
+    imports: [HttpClientModule, CommonModule],
+    providers: [PersonService]
+})
+export class PersonsComponent implements OnInit {
+    users: User[] = []
+    constructor(private readonly personService: PersonService) { }
+
+    ngOnInit(): void {
+        this.personService.get(1, 10).subscribe({
+            next: (response) => {
+                this.users = response.data
+            },
+            error: (e: HttpErrorResponse) => {
+                console.log('An unexpected error =>', e.error)
+            }
+        })
+    }
+
+}
