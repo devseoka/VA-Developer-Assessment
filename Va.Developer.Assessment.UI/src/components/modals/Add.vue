528ca6234d543eb8f825b918c94036e92f93c732
Moshele Seoka <seokamk@yahoo.com>
2025-02-22 14:40:48 +0200

    Feat: Center align success and error messages in Add component modal

--- Va.Developer.Assessment.UI/src/components/modals/Add.vue
+++ Va.Developer.Assessment.UI/src/components/modals/Add.vue
@@ -21,7 +21,7 @@
         <!-- Modal body -->
         <form class="p-4 md:p-5" @submit.prevent="onSubmit">
           <div class="grid gap-4 mb-4 grid-cols-2">
-            <div v-if="messages.length > 0" class="col-span-2">
+            <div v-if="messages.length > 0" class="col-span-2 text-center">
               <p v-for="(message, index) in messages" :key="index" :class="succeeded ?
                 'mt-2 text-sm text-assessment-accent-600 capitalize'
                 : 'mt-2 text-sm text-assessment-primary-600 capitalize'">
